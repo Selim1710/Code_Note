973795f0e198f866f4d97beb491d8644e5de4de8
Selim1710 <shamimaldeen@gmail.com>
2023-06-14 17:39:51 +0600

    env setup

--- env_setup.php
+++ env_setup.php
@@ -1,7 +1,14 @@
 <?php
 
-// 1 - config >> app.php: 'asset_url' => env('ASSET_URL', 'https://www.example.com'),
-// command: php artisan config:cache
+/* No-1:
+    config >> app.php: 'asset_url' => env('ASSET_URL', 'https://www.example.com'),
+    command: php artisan config:cache
 
-// 2 - ASSET_URL=public
+*/
+    
 
+/* No-2: 
+    APP_URL=http://localhost/hazira-v2/public
+    ASSET_URL=/hazira-v2/public
+    LOCAL_STORAGE_PREFIX=/public
+*/
