0dd5e02cb97cc74ae6dff326ff43599243301659
Selim1710 <shamimaldeen@gmail.com>
2023-06-20 18:05:31 +0600

    laravel model command

--- Laravel_Command.php
+++ Laravel_Command.php
@@ -1,3 +1,12 @@
 <?php
 
 # 1. composer remove vendor/packageName
+
+# 2. php artisan make:model ModelName -mcr 
+
+# 3. php artisan make:model ModelName -mcr --api  // api resource controller
+
+# 4. php artisan make:model ModelName -mcr -R  // with Request
+
+# 5. php artisan make:model ModelName -a  // model,migration,factory
+
