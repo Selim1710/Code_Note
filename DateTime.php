283a9bf4babd2acdf6d82de8250a7a68f0b02857
Selim1710 <shamimaldeen@gmail.com>
2023-07-15 18:32:30 +0600

    date time update

--- DateTime.php
+++ DateTime.php
@@ -10,16 +10,28 @@
 
     $total_working_hour = $total_time;
 
+*/
 
+/*
     //  add hour and subtract hour
     $date_time = '2023-07-15 10:00:00';
     $DateTime1 = new DateTime($date_time);
     $DateTime1->modify('+2 hours'); or $DateTime1->modify('-2 hours');
     $final_shift_end_time = $date . ' ' . $DateTime1->format("H:i:s");
+    
+    */
+
+    /*
 
     // subtract day in php
     $day = date('Y-m-d', strtotime('-7 days'))
 
+    */
+
+
+    /*
 
     // Date-Time Local Value showing
     <input type="datetime-local" name="date_start" value="{{ date('Y-m-d\TH:i', strtotime($yourPassedVariableToView)) }}">
+
+    */
