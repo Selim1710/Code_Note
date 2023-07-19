3b873e522aebb258cd6c247941c6ed8da4fafbf5
Selim1710 <shamimaldeen@gmail.com>
2023-07-19 17:31:33 +0600

    day name

--- DateTime.php
+++ DateTime.php
@@ -1,6 +1,6 @@
 <?php
     /*
-    //  Calculate duration between two times 
+    //////////  Calculate duration between two times  ////////
 
     $time1 = new DateTime($emp_entry);
     $time2 = new DateTime($emp_exit);
@@ -8,28 +8,45 @@
     $total_time = $time_difference->format('%H:%I:%S');
 
     $total_working_hour = $total_time;
-
-    */
+   
 
 
     /*
-    //  add hour and subtract hour
+    ////////  add hour and subtract hour  ////////////////
+
     $date_time = '2023-07-15 10:00:00';
     $DateTime1 = new DateTime($date_time);
     $DateTime1->modify('+2 hours'); or $DateTime1->modify('-2 hours');
     $final_shift_end_time = $date . ' ' . $DateTime1->format("H:i:s");
     
-    */
-
     /*
-    // subtract day in php
+    ////////// subtract day in php ////////
+
     $day = date('Y-m-d', strtotime('-7 days'))
 
-    */
 
 
     /*
     // Date-Time Local Value showing
+
     <input type="datetime-local" name="date_start" value="{{ date('Y-m-d\TH:i', strtotime($yourPassedVariableToView)) }}">
 
+
+    /*
+    // Week Name: Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday, Friday 
+    
+    $week_name = date("l");
+    
+    // Week Name: Sat, Sun, Mon, Tue, Wed, Thu, Fri 
+    
+    $week_name = date("D");
+
+
+    /*
+    // Month Name:
+    $month_name = date('F'); // December
+    $month_name = date('M'); // Dec
+    
+    
+    --- the end ---
     */
