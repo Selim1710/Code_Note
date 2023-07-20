9b54baef45d643e6a912a144e2d3db981952040d
Selim1710 <shamimaldeen@gmail.com>
2023-07-20 12:52:21 +0600

    number formatter

--- Number_To_Word.php
+++ Number_To_Word.php
@@ -1,6 +1,8 @@
-<?php 
+<?php
 
- $number = 1500;
+$number = 1500;
 
- $digit = new NumberFormatter("en", NumberFormatter::SPELLOUT);
- return $digit->format($number);
+$digit = new NumberFormatter("en", NumberFormatter::SPELLOUT);
+return $digit->format($number);
+
+//  Note: extension=php_intl.dll - must have into php.ini file
