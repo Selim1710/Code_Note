0d35320598f71f46dbdea9596b6298315cbdcfdb
Selim1710 <shamimaldeen@gmail.com>
2023-08-05 15:02:13 +0600

    jqury cdn add

--- Print.php
+++ Print.php
@@ -7,6 +7,7 @@
 
 
 <!-- js script -->
+<script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
 <script src="{{ asset('js/Print.js') }}" type="text/javascript"></script>
 <script type="text/javascript">
     $('#click_print').click(function() {
