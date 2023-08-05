cd90eeb3db591c622b3736e6a039c17532cd6732
Selim1710 <shamimaldeen@gmail.com>
2023-08-05 17:13:32 +0600

    footer fixed bottom

---
+++ Footer_fixed_bottom.php
@@ -0,0 +1,14 @@
+  <!-- footer -->
+  <style>
+      .footer {
+          position: fixed;
+          left: 0;
+          bottom: 0;
+          width: 100%;
+          margin-top: 10px;
+      }
+  </style>
+  <div class="footer">
+      <span style="margin-left:10px;">© Developed By Acquaint Technologies</span>
+      <img src="images/logo.png" style="height:40px;width:50px;float:right;">
+  </div>
