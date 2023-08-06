b15677050159516c8edb8e8297a6cca0f734a35f
Selim1710 <shamimaldeen@gmail.com>
2023-08-06 17:40:28 +0600

    footer 2

--- Footer_fixed_bottom.php
+++ Footer_fixed_bottom.php
@@ -1,4 +1,4 @@
-  <!-- footer -->
+  <!-- footer-1 -->
   <style>
       .footer {
           position: fixed;
@@ -12,3 +12,27 @@
       <span style="margin-left:10px;">© Developed By Acquaint Technologies</span>
       <img src="images/logo.png" style="height:40px;width:50px;float:right;">
   </div>
+
+  <!-- footer-2 -->
+  <style>
+        html {
+            position: relative;
+        }
+        body {
+            margin: 0 0 100px 0;
+            padding: 25px;
+        }
+        footer {
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            overflow: hidden;
+        }
+    </style>
+    <footer>
+        <span>
+            {{ $general_setting->address ?? '' }}
+        </span>
+        <img src="{{ asset('public/images/office.png') }}" style="height:40px;width:50px;float:right;">
+    </footer>
