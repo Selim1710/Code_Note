fe16441fc82df9a42befb5a9f16d937a9c2a4f62
Selim1710 <shamimaldeen@gmail.com>
2023-10-30 12:49:02 +0600

    payment type calculation-bank/cash

---
+++ Data-Table.php
@@ -0,0 +1,120 @@
+<?php
+    /*
+    //////////  data-table ////////
+
+    // code
+    <script type="text/javascript">
+        $("#print-btn").on("click", function() {
+            var divToPrint = document.getElementById('quotation-details');
+            var newWin = window.open('', 'Print-Window');
+            newWin.document.open();
+            newWin.document.write(
+                '<link rel="stylesheet" href="<?php echo asset('public/vendor/bootstrap/css/bootstrap.min.css'); ?>" type="text/css"><style type="text/css">@media print {.modal-dialog { max-width: 1000px;} }</style><body onload="window.print()">' +
+                divToPrint.innerHTML + '</body>');
+            newWin.document.close();
+            setTimeout(function() {
+                newWin.close();
+            }, 10);
+        });
+
+        $('#quotation-table').DataTable({
+            "order": [],
+            'language': {
+                'lengthMenu': '_MENU_ {{ trans('file.records per page') }}',
+                "info": '<small>{{ trans('file.Showing') }} _START_ - _END_ (_TOTAL_)</small>',
+                "search": '{{ trans('file.Search') }}',
+                'paginate': {
+                    'previous': '<i class="dripicons-chevron-left"></i>',
+                    'next': '<i class="dripicons-chevron-right"></i>'
+                }
+            },
+            'columnDefs': [{
+                    "orderable": false,
+                    'targets': [0, 2]
+                },
+
+            ],
+
+            // 'searching': false,
+            // 'paging': false,
+
+            'select': {
+                style: 'multi',
+                selector: 'td:first-child'
+            },
+            'lengthMenu': [
+                [10, 25, 50, -1],
+                [10, 25, 50, "All"]
+            ],
+            dom: '<"row"lfB>rtip',
+            buttons: [{
+                    extend: 'pdf',
+                    text: '{{ trans('file.PDF') }}',
+                    title: 'Payment ledger',
+                    // for 100% width of table
+                    customize: function(doc) {
+                        doc.content[1].table.widths =
+                            Array(doc.content[1].table.body[0].length + 1).join('*').split('');
+
+                    },
+                    exportOptions: {
+                        columns: ':visible:Not(.not-exported)',
+                        rows: ':visible'
+                    },
+                    action: function(e, dt, button, config) {
+                        datatable_sum(dt, true);
+                        $.fn.dataTable.ext.buttons.pdfHtml5.action.call(this, e, dt, button, config);
+                        datatable_sum(dt, false);
+                    },
+                    footer: true
+                },
+                {
+                    extend: 'csv',
+                    text: '{{ trans('file.CSV') }}',
+                    exportOptions: {
+                        columns: ':visible:Not(.not-exported)',
+                        rows: ':visible'
+                    },
+                    action: function(e, dt, button, config) {
+                        datatable_sum(dt, true);
+                        $.fn.dataTable.ext.buttons.csvHtml5.action.call(this, e, dt, button, config);
+                        datatable_sum(dt, false);
+                    },
+                    footer: true
+                },
+                {
+                    extend: 'print',
+                    text: '{{ trans('file.Print') }}',
+                    title: 'Payment ledger',
+                    // decrease h1 size
+                    customize: function(doc) {
+                        $(doc.document.body).find('h1').css('font-size', '15pt');
+                        $(doc.document.body).find('h1').css('text-align', 'center');
+                    },
+                    exportOptions: {
+                        columns: ':visible:Not(.not-exported)',
+                        rows: ':visible'
+                    },
+                    action: function(e, dt, button, config) {
+                        datatable_sum(dt, true);
+                        $.fn.dataTable.ext.buttons.print.action.call(this, e, dt, button, config);
+                        datatable_sum(dt, false);
+                    },
+                    footer: true
+                },
+
+                {
+                    extend: 'colvis',
+                    text: '{{ trans('file.Column visibility') }}',
+                    columns: ':gt(0)'
+                },
+            ],
+            drawCallback: function() {
+                var api = this.api();
+                datatable_sum(api, false);
+            }
+        });
+    </script>
+
+    
+    */
--- Form.php
+++ Form.php
@@ -2,10 +2,7 @@
 
 /*
 
-
-
 // login with mobile username and email in laravel
-
 public function index(Request $request)
 {
     if (is_numeric($request->email)) {
@@ -56,8 +53,6 @@ public function index(Request $request)
   </script>
 
 //////////////// Problem: Multiple autocomplete in laravel  ////////////////
-
-
 // Blade:
 
 // {{-- description --}}
@@ -137,51 +132,6 @@ script:
 
  // route:
  Route::get('/autocomplete/search', 'RemainderController@autoCompleteSearch')->name('remainder.autocomplete-search');
-
-
-
-
-////////////// Problem: add more input field using jquery  //////////////
-
-// step-1:
-
-<div class="row" id="academic_document">
-  <div class="col-12 d-flex">
-  <input class="form-control" type="text" name="academic_documentation[0]">
- <input class="form-control ml-1" type="file" name="academic_documentation_file[0]">
- <button type="button" class="btn btn-primary ml-1 btn-sm" id="add_more_button">Add more</button>
-      </div>
-   </div>
-
-// step-2:
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-<script>
-    $(document).ready(function() {
-        var i = 1;
-        $('#add_more_button').click(function() {
-            var number = i++;
-
-            var new_input = "<div class='col-12 mt-3 d-flex' id='academic_document'>" +
-                "<input class='form-control' type='text' name='academic_documentation[" + number +
-                "]'>" +
-                "<input class='form-control ml-2' type='file' name='academic_documentation_file[" +
-                number + "]'>" +
-                "<button type='button' class='btn btn-danger ml-2 btn-sm' id='remove_button'>" +
-                "Remove" +
-                "</button>" +
-                "</div>";
-            $('#academic_document').append(new_input);
-        });
-
-
-        $(document).on('click', '#remove_button', function() {
-            $(this).parent('div').remove();
-        });
-
-
-    });
-</script>
-
 
 
 
@@ -353,28 +303,256 @@ public function subDeptWiseEmployeeAjaxSearch(Request $request)
 }
 
 
-/////////////////////////////////  add new field in ajax  /////////////////////////////////
+////////////// Problem: add more input field using jquery  //////////////
 
+// step-1:
+
+<div class="row" id="academic_document">
+  <div class="col-12 d-flex">
+  <input class="form-control" type="text" name="academic_documentation[0]">
+ <input class="form-control ml-1" type="file" name="academic_documentation_file[0]">
+ <button type="button" class="btn btn-primary ml-1 btn-sm" id="add_more_button">Add more</button>
+      </div>
+   </div>
+
+// step-2:
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script>
     $(document).ready(function() {
-        var addBtn = $('.new_add');
-        $(addBtn).click(function() {
-            var maxField = 15;
-            var wrapper = $('.addtbody');
-            var x = 1;
-            var fieldHtml =
-                '<tr id="voucher_information"  class=""><td class=""><input type="text" class="form-control" name="documents_name[]" ></td><td class=""><input type="file" class="form-control" name="documents_file[]" ></td><td><button type="button" class="btn btn-danger btn-sm delete_class remove">X</button></td></tr>';
-            if (x < maxField) {
-                x++;
-                $(wrapper).append(fieldHtml);
-            }
-        })
-    });
+        var i = 1;
+        $('#add_more_button').click(function() {
+            var number = i++;
 
-    $('tbody').on('click', '.remove', function() {
-        $(this).parent().parent().remove();
-    });
+            var new_input = "<div class='col-12 mt-3 d-flex' id='academic_document'>" +
+                "<input class='form-control' type='text' name='academic_documentation[" + number +
+                "]'>" +
+                "<input class='form-control ml-2' type='file' name='academic_documentation_file[" +
+                number + "]'>" +
+                "<button type='button' class='btn btn-danger ml-2 btn-sm' id='remove_button'>" +
+                "Remove" +
+                "</button>" +
+                "</div>";
+            $('#academic_document').append(new_input);
+        });
 
+
+        $(document).on('click', '#remove_button', function() {
+            $(this).parent('div').remove();
+        });
+
+
+    });
 </script>
 
+
+/////////////////////////////////  add more with calculation  /////////////////////////////////
+
+<script>
+
+    var counter = 0;
+    $("#add_more_btn").on('click', function(e) {
+        e.preventDefault();
+
+        ++counter;
+        // console.log(counter);
+
+        if (counter == 3) {
+            alert('You can add maximum 3 row');
+        } else {
+            var html =
+                '<tr><td><input type="text"name="note[' + counter +
+                ']"class="form-control note_' + counter + '"></td><td><input type ="text"name="unit[' +
+                counter +
+                ']"class="form-control unit_' + counter + '"></td><td><input type="text"name="total_amount[' +
+                counter +
+                ']"class ="form-control total_amount_' + counter +
+                ' amount"readonly></td><td><button type="button" class="btn btn-danger btn-sm delete_class remove">X</button></td></tr>';
+
+            $("#wrapper").append(html);
+
+            $(document).on('click', '.remove', function() {
+                $(this).parents('tr').remove();
+            });
+        }
+
+
+        $(".note_" + counter).on('keyup', function() {
+            var note = $(".note_" + counter).val();
+            // console.log(note);
+
+            if (note == '') {
+                note = 0;
+            }
+
+            var unit = $(".unit_" + counter).val();
+            if (unit == '') {
+                unit = 0;
+            }
+
+            var total_amount = parseFloat(note) * parseFloat(unit);
+            $(".total_amount_" + counter).val(total_amount);
+
+            // grand_total
+            var total = 0;
+            $(".amount").each(function() {
+                total += parseFloat($(this).val());
+            });
+            $("#grand_total").val(total);
+
+        });
+
+        $(".unit_" + counter).on('keyup', function() {
+            var note = $(".note_" + counter).val();
+            // console.log('note ' + note);
+
+            if (note == '') {
+                note = 0;
+            }
+
+            var unit = $(".unit_" + counter).val();
+            if (unit == '') {
+                unit = 0;
+            }
+
+            // console.log('unit ' + unit);
+
+            var total_amount = parseFloat(note) * parseFloat(unit);
+            $(".total_amount_" + counter).val(total_amount);
+
+            // grand_total
+            var total = 0;
+            $(".amount").each(function() {
+                total += parseFloat($(this).val());
+            });
+            $("#grand_total").val(total);
+
+        });
+    });
+</script>
+
+/////////////////////////// index no calculation ///////////////////////////
+
+<script>
+    // total_amount, note, unit
+    $(".note,.unit").on('keyup', function() {
+        var index_no = $(this).data('index_no');
+
+        var note = $("#note_" + index_no).val();
+        // alert(note);
+
+        if (note == '') {
+            note = 0;
+        }
+
+        var unit = $("#unit_" + index_no).val();
+        if (unit == '') {
+            unit = 0;
+        }
+
+        var total_amount = parseFloat(note) * parseFloat(unit);
+        $("#total_amount_" + index_no).val(total_amount);
+
+        // grand_total
+        var total = 0;
+        $(".amount").each(function() {
+            total += parseFloat($(this).val());
+        });
+        $("#grand_total").val(total);
+
+    });
+</script>
+
+
+///////////////////////////// payment type calculation ///////////////////////////
+<div class="col-3">
+    <div class="form-group">
+        <label>Payment type<span class="text-danger">*</span></label>
+        <select name="payment_type" id="paid_by" class="form-control text-capitalize">
+            <option value="1"
+                @if (isset($payment->payment_type)) {{ $payment->payment_type == 1 ? 'selected' : '' }} @endif>
+                Cash
+            </option>
+            <option value="2"
+                @if (isset($payment->payment_type)) {{ $payment->payment_type == 2 ? 'selected' : '' }} @endif>
+                Bank
+            </option>
+
+        </select>
+    </div>
+    </div>
+
+    <div class="col-3 banking">
+    <div class="form-group">
+        <label class="text-capitalize">
+            Cheque Number
+        </label>
+        <input type="text" name="cheque_no" class="form-control cheque_no"
+            value="{{ $payment->cheque_no ?? '' }}">
+    </div>
+    </div>
+    <div class="col-3 banking">
+    <div class="form-group">
+        <label class="text-capitalize">
+            Accounts
+        </label>
+        <select name="account_id" id="account_id" class="form-control">
+            @forelse ($accounts as $account)
+                <option value="{{ $account->id }}"
+                    @if (isset($payment->account_id)) {{ $payment->account_id == $account->id ? 'selected' : '' }} @endif>
+                    {{ $account->name }}</option>
+            @empty
+                <option value="">No data found</option>
+            @endforelse
+
+        </select>
+    </div>
+</div>
+
+// script
+<script>
+    $(document).ready(function() {
+        var paid_by = $('#paid_by').find('option:selected').val();
+
+        // paid - by cash
+        if (paid_by == 1) {
+            $('.banking').hide();
+
+        }
+        // paid - by bank
+        else if (paid_by == 2) {
+            $('.banking').show();
+
+        }
+        // paid - by bank
+        else {
+            $('.banking').hide();
+        }
+
+        // cash , cheque, bkash....
+        $('#paid_by').on('change', function() {
+            var paid_by = $(this).val();
+            // cash
+            if (paid_by == 1) {
+                $('.banking').hide();
+                $('.bank_name').val('');
+                $('.cheque_no').val('');
+                $('#account_id').val('');
+            }
+            // cheque
+            else if (paid_by == 2) {
+                $('.banking').show();
+            }
+            // mobile banking
+            else {
+                $('.banking').hide();
+                $('.bank_name').val('');
+                $('.branch_name').val('');
+                $('.cheque_no').val('');
+                $('#account_id').val('');
+            }
+        });
+    });
+</script>
+
+// end payment type calculation
 
