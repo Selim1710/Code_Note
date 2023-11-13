d38279f4cb383039daa187d1252885a8fe166715
Selim1710 <shamimaldeen@gmail.com>
2023-11-13 14:58:30 +0600

    file size validation

--- Form_Validation.php
+++ Form_Validation.php
@@ -19,3 +19,22 @@ $request->validate([
 $request->validate([
     'email' => 'unique:table_name,email,' . $user->id
 ]);
+
+
+
+// <!-- file size validation using jquery -->
+<script>
+    $(document).ready(function() {
+        $('#image').bind('change', function() {
+            var file_size = (this.files[0].size);
+            var maximum_file_size = Math.round(file_size / (1024 * 1024)); // for mb
+            // console.log('maximum_file_size: ' + maximum_file_size);
+
+            // 2mb validation
+            if (maximum_file_size > 2) {
+                alert('large');
+                $('#image').val('');
+            };
+        });
+    });
+</script>
