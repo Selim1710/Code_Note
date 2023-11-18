c96c326cab4b621e89aa2e6985dd632f46f3b3d9
Selim1710 <shamimaldeen@gmail.com>
2023-11-18 13:07:36 +0600

    redirect in c-panel

---
+++ Redirect-Cpanel.php
@@ -0,0 +1,17 @@
+<?php
+/*
+
+<!-- Redirect c-panel project -->
+
+* step-1: goto old project folder and create index.php file
+* step-2: goto index.php
+* copy and paste:
+
+<?php
+header('Location: https://ziarah.biz/consultancy/');
+exit;
+?>
+
+
+
+*/
