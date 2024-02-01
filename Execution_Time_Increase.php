7bf432ab428c11c2308b02fac3b09b21ca004001
Selim1710 <shamimaldeen@gmail.com>
2024-02-01 14:48:06 +0600

    maximum execution time update

---
+++ Execution_Time_Increase.php
@@ -0,0 +1,14 @@
+<?php
+
+/***
+ * Goto controller:
+ * copy and paste below code: 
+ 
+   public function __construct()
+    {
+        ini_set('max_execution_time', '300'); //300 seconds = 5 minutes
+        ini_set('max_execution_time', '0'); // for infinite time of execution 
+    }
+
+    
+ */
