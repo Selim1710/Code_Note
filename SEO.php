696d0a481eab1b60c306e8bebfabfef28c56f1c9
Selim1710 <shamimaldeen@gmail.com>
2024-04-17 10:57:18 +0600

    seo

---
+++ SEO.php
@@ -0,0 +1,7 @@
+
+
+
+<title>@yield('title','Corporate Office E-commarce Website')</title>
+<meta name="keywords" content="@yield('meta_keywords','Corporate Office')">
+<meta name="description" content="@yield('meta_description','Corporate Office')">
+<link rel="canonical" href="@yield('canonical_url','Corporate Office')" />
