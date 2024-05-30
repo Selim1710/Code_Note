6264b464e9bc0d94993070dc3412c1cb45c4cb34
Selim1710 <shamimaldeen@gmail.com>
2024-05-30 14:16:16 +0600

    google map adding

---
+++ GoogleMap.php
@@ -0,0 +1,15 @@
+<?php
+/*
+
+* go to google map
+* search a location 
+* click on share button
+* then go to embaded a map
+* copy the link and paste it into laravel blade file:
+
+#code:
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3652.4403499359655!2d90.40978887409968!3d23.731671589497427!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b96b4c60653f%3A0x9143a6ffc8d4892c!2sFahima%20Tower!5e0!3m2!1sen!2sbd!4v1717056812685!5m2!1sen!2sbd" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+    
+*/
