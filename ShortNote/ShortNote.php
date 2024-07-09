c6a8c98168989cdcac1a26ff14a1c6274ce811f7
Selim1710 <shamimaldeen@gmail.com>
2024-07-09 14:54:43 +0600

    month array

--- ShortNote/ShortNote.php
+++ ShortNote/ShortNote.php
@@ -4,6 +4,31 @@
 // limit string
 {!! Str::limit($item->description, 30) !!}
 
+// month array
 
+    @php
+        $arraymonths = [
+            '1' => 'January',
+            '2' => 'February',
+            '3' => 'March',
+            '4' => 'April',
+            '5' => 'May',
+            '6' => 'June',
+            '7' => 'July',
+            '8' => 'August',
+            '9' => 'September',
+            '10' => 'October',
+            '11' => 'November',
+            '12' => 'December',
+        ];
+    @endphp
+    @if(!empty($arraymonths))
+        @foreach ($arraymonths as $key => $month)
+            <option value="{{ $key }}"
+                {{ $month == date('F') ? 'Selected' : '' }}>
+                {{ $month }}
+            </option>
+        @endforeach
+    @endif
 
 ?>
