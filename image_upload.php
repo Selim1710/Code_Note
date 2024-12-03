9aa6562460405225688533a06d135e2464b0162b
Md. Selim Hossain Suhag <93088169+Selim1710@users.noreply.github.com>
2024-12-03 16:17:08 +0600

    Update image_upload.php

--- image_upload.php
+++ image_upload.php
@@ -20,39 +20,39 @@ if ($request->hasfile('image')) {
 /////////////////// multiple image upload  ///////////////////
 
 if (!empty($request->degree)) {
-                    foreach ($request->degree as $index => $degree) {
-                        // if file exist
-                        if (isset($request->file('file')[$index])) {
-                            // return 'file';
-                            $file = $request->file('file')[$index];
-
-                            if ($file) {
-                                File::delete(public_path('/uploads/users/education_file/' . $file->getClientOriginalName()));
-                                $edu_filename = date('Ymdmhs') . '.' . $file->getClientOriginalExtension();
-                                $file->move(public_path('uploads/users/education_file'), $edu_filename);
-
-                                UserEducation::create([
-                                    'user_id' => $user->id,
-                                    'degree' => $degree,
-                                    'institute' => $request->institute[$index] ?? null,
-                                    'board' => $request->board[$index] ?? null,
-                                    'passing_year' => $request->passing_year[$index] ?? null,
-                                    'result' => $request->result[$index] ?? null,
-                                    'file' => $edu_filename,
-                                ]);
-                            }
-                        } else {
-                            // return 'no file';
-                            $edu_filename = $request->file[$index] ?? '';
-                            UserEducation::create([
-                                'user_id' => $user->id,
-                                'degree' => $degree,
-                                'institute' => $request->institute[$index] ?? null,
-                                'board' => $request->board[$index] ?? null,
-                                'passing_year' => $request->passing_year[$index] ?? null,
-                                'result' => $request->result[$index] ?? null,
-                                'file' => $edu_filename ?? null,
-                            ]);
-                        }
-                    }
-                }
+    foreach ($request->degree as $index => $degree) {
+        // if file exist
+        if (isset($request->file('file')[$index])) {
+            // return 'file';
+            $file = $request->file('file')[$index];
+    
+            if ($file) {
+                File::delete(public_path('/uploads/users/education_file/' . $file->getClientOriginalName()));
+                $edu_filename = date('Ymdmhs') . '.' . $file->getClientOriginalExtension();
+                $file->move(public_path('uploads/users/education_file'), $edu_filename);
+    
+                UserEducation::create([
+                    'user_id' => $user->id,
+                    'degree' => $degree,
+                    'institute' => $request->institute[$index] ?? null,
+                    'board' => $request->board[$index] ?? null,
+                    'passing_year' => $request->passing_year[$index] ?? null,
+                    'result' => $request->result[$index] ?? null,
+                    'file' => $edu_filename,
+                ]);
+            }
+        } else {
+            // return 'no file';
+            $edu_filename = $request->file[$index] ?? '';
+            UserEducation::create([
+                'user_id' => $user->id,
+                'degree' => $degree,
+                'institute' => $request->institute[$index] ?? null,
+                'board' => $request->board[$index] ?? null,
+                'passing_year' => $request->passing_year[$index] ?? null,
+                'result' => $request->result[$index] ?? null,
+                'file' => $edu_filename ?? null,
+            ]);
+        }
+    }
+}
